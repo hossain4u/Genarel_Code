410fce6dd8c629b5c434d4c438a291c15e68da3d
hossain4u <sujonmazi20@gmail.com>
2025-07-13 21:29:41 +0600

    update chada file

--- codes/bin/basic_codes/chada.dart
+++ codes/bin/basic_codes/chada.dart
@@ -3,30 +3,34 @@ import 'dart:io';
 void main(){
 print('Enter your name:');
 String? soinikName =stdin.readLineSync();
-print('Soinik name is : $soinikName');
 
-print('Enter your income');
-String? soinikIncome =stdin.readLineSync(); 
-int? income =int.parse(soinikIncome!);
-print('Soinik income:$income taka');
 
-double chada = (income.toDouble()*0.10);
-print('Sonik monthly chada:$chada taka');
-
-print('Date: $DateTime.now()');
-
-if(chada >=100.00 && chada <= 4999.00){
-print('Deshneta reaction: unsatisfied! chada is very low');
-}else if(chada >=5000.00 && chada <= 9999.00){
-print('Deshneta reaction: average! not good');
-}else if(chada >=10000.00 && chada <= 1999.00){
-print('Deshneta reaction: Its ok! but not high');
-}else if(chada >=20000.00 && chada <= 2999.00){
-print('Deshneta reaction: Well done');
-}else{
-  print('Deshneta reaction: Deshneta Khus howa!!');
+if(soinikName == null || soinikName.isNotEmpty){
+  print('No data provided');
+  return;
 }
 
-print('/n VERIFIED BY POLTON');
+print('Enter your income');
+String? soinikIncome =stdin.readLineSync();
+
+int? income =int.parse(soinikIncome!);
+double chada = (income*0.10);
+print('Sonik monthly chada: ${chada.toDouble()} taka');
+double netIcome =(income-chada);
+print('10% buje neyar por: $netIcome taka');
+
+if(chada >= 1.00 && chada <= 1999.00){
+print('Jononetar massage: unsatisfied! chada is very low');
+}else if(chada >=2000.00 && chada <= 2999.00){
+print('Jononetar massage: Not bed');
+}else if(chada >=3000.00 &&  chada <= 3999.00){
+print('Jononetar massage: Its ok!');
+}else if(chada >=4000.00 && chada <= 5000.00){
+print('Jononetar massage: Well done $soinikName');
+}else{
+  print('Jononater massage: Jononeta Khus huya!!');
+}
+print('Date:${DateTime.now().toString().split('.').first}');
+print('\nVERIFIED BY POLTON');
 
 }
